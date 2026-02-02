c6846468c73abd347869a17b106277e3c4989ef4
Sourav <thakursourav778@gmail.com>
2026-02-02 21:39:33 +0530

    day 10

--- day9/frontend/vite-project/src/App.jsx
+++ day9/frontend/vite-project/src/App.jsx
@@ -1,37 +1,65 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import axios from "axios"
 
 
 function App() {
-  const [notes, setNotes] = useState([
-    {
-      title:"Note",
-      description:"notessss"
-    },
-    {
-      title:"Note",
-      description:"notessss"
-    },
-    {
-      title:"Note",
-      description:"notessss"
-    },
-    {
-      title:"Note",
-      description:"notessss"
-    }
-  ])
+  const [notes, setNotes] = useState([])
 
-  axios.get('').then((res)=>{
-    const notes = res.notes
-  })
+  function fetchNotes(){
+    axios.get('http://localhost:3000/api/notes')
+    .then((res)=>{
+      setNotes(res.data.notes)
+    })
+  }
+
+  useEffect(()=>{
+    fetchNotes()
+  },[])
   
+  
+  function handelSubmit(e){
+    e.preventDefault()
+    const {title, description} = e.target.elements
+    console.log(title.value, description.value)
+
+    axios.post("http://localhost:3000/api/notes",{
+      title: title.value,
+      description: description.value
+    }).then((res)=>{
+      console.log(res);
+      fetchNotes()
+    })
+  }
+  
+  function handelDeleteNote(noteId){
+    // axios.delete("http://localhost:3000/api/note/"+noteId)
+    // .then((res)=>{
+    //   console.log(res);
+      
+    // })
+
+    console.log(noteId)
+  }
   
 
 
   return(
     <>
-      <h1>hello</h1>
+      <form className='note-create-form' onSubmit={handelSubmit}>
+        <input type="text" name='title'/>
+        <input type="text" name='description'/>
+        <button>Submit</button>
+      </form>
+
+      <div className="notes">
+        {notes.map((note)=>{
+          return<div className="card">
+            <h1>{note.title}</h1>
+            <p>{note.description}</p>
+            <button onClick={handelDeleteNote(note._id)}>Delete</button>
+          </div>
+        })}
+      </div>
     </>
   )
 }
