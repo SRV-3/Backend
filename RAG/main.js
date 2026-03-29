4eda826d5a35dbd9a6854ed8cd720f9ea1445e72
Sourav <thakursourav778@gmail.com>
2026-03-29 15:23:07 +0530

    Add initial implementation for RAG with PDF parsing and Pinecone integration

---
+++ RAG/main.js
@@ -0,0 +1,52 @@
+import "dotenv/config";
+import { PDFParse } from "pdf-parse";
+import { Pinecone } from "@pinecone-database/pinecone";
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import { MistralAIEmbeddings } from "@langchain/mistralai";
+import fs from "fs";
+
+const pc = new Pinecone({
+  apiKey: process.env.PINECONE_API_KEY,
+});
+const index = pc.Index("cohort-2-rag");
+
+let dataBuffer = fs.readFileSync("./story.pdf");
+
+const parser = new PDFParse({
+  data: dataBuffer,
+});
+
+const data = await parser.getText();
+const embedbings = new MistralAIEmbeddings({
+  apiKey: process.env.MISTRAL_API_KEY,
+  model: "mistral-embed",
+});
+
+const splitter = new RecursiveCharacterTextSplitter({
+  chunkSize: 500,
+  chunkOverlap: 0,
+});
+
+const chunks = await splitter.splitText(data.text);
+
+const docs = await Promise.all(
+  chunks.map(async (chunk) => {
+    const embedbing = await embedbings.embedQuery(chunk);
+    return {
+      text: chunk,
+      embedbing,
+    };
+  }),
+);
+
+const results = await index.upsert({
+  records: docs.map((doc, i) => ({
+    id: `doc-${i}`,
+    values: doc.embedbing,
+    metadata: {
+      text: doc.text,
+    },
+  })),
+});
+
+console.log(results);
