1b63a0714acfc471f5031415ede8d75c63ce786a
Sourav <thakursourav778@gmail.com>
2026-03-29 15:32:42 +0530

    Refactor main.js to comment out unused PDF parsing code and add query embedding functionality; create new untitled file for query results

--- RAG/main.js
+++ RAG/main.js
@@ -10,43 +10,51 @@ const pc = new Pinecone({
 });
 const index = pc.Index("cohort-2-rag");
 
-let dataBuffer = fs.readFileSync("./story.pdf");
+// let dataBuffer = fs.readFileSync("./story.pdf");
 
-const parser = new PDFParse({
-  data: dataBuffer,
-});
+// const parser = new PDFParse({
+//   data: dataBuffer,
+// });
 
-const data = await parser.getText();
+// const data = await parser.getText();
+
 const embedbings = new MistralAIEmbeddings({
   apiKey: process.env.MISTRAL_API_KEY,
   model: "mistral-embed",
 });
 
-const splitter = new RecursiveCharacterTextSplitter({
-  chunkSize: 500,
-  chunkOverlap: 0,
+// const splitter = new RecursiveCharacterTextSplitter({
+//   chunkSize: 500,
+//   chunkOverlap: 0,
+// });
+
+// const chunks = await splitter.splitText(data.text);
+
+// const docs = await Promise.all(
+//   chunks.map(async (chunk) => {
+//     const embedbing = await embedbings.embedQuery(chunk);
+//     return {
+//       text: chunk,
+//       embedbing,
+//     };
+//   }),
+// );
+
+// const results = await index.upsert({
+//   records: docs.map((doc, i) => ({
+//     id: `doc-${i}`,
+//     values: doc.embedbing,
+//     metadata: {
+//       text: doc.text,
+//     },
+//   })),
+// });
+
+const queryEmbedding = await embedbings.embedQuery("how was the intership?");
+const results = await index.query({
+  vector: queryEmbedding,
+  topK: 2,
+  includeMetadata: true,
 });
 
-const chunks = await splitter.splitText(data.text);
-
-const docs = await Promise.all(
-  chunks.map(async (chunk) => {
-    const embedbing = await embedbings.embedQuery(chunk);
-    return {
-      text: chunk,
-      embedbing,
-    };
-  }),
-);
-
-const results = await index.upsert({
-  records: docs.map((doc, i) => ({
-    id: `doc-${i}`,
-    values: doc.embedbing,
-    metadata: {
-      text: doc.text,
-    },
-  })),
-});
-
-console.log(results);
+console.log(JSON.stringify(results));
