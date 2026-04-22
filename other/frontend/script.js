b6621b4f235ca4dd9ff07786b06676ee7534133d
Sourav Singh <thakursourav778@gmail.com>
2026-04-22 23:42:41 +0530

    Update script.js

--- other/frontend/script.js
+++ other/frontend/script.js
@@ -2,6 +2,5 @@ const pointer = document.querySelector('.mouse-follower')
 
 document.body.addEventListener('mouseleave', (e)=>{
     const {clientX, clientY} = e
-    
     pointer.style.transform = `translate(${clientX}px, ${clientY}px,)`
 })
